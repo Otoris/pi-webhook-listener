b404018279b62790b2b74367fcece01d850fff87
Ethan Trawick <ethan.trawick@me.com>
2019-03-01 03:04:12 -0500

    Add express server and default routes

---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var exec = require('child_process').exec;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/github', function (req, res) {
+  res.sendStatus(200);
+  console.log('GET /github');
+});
+
+app.post('/github', function (req, res) {
+  console.log(req.body.pusher.name + ' pushed to ' + req.body.repository.name);
+
+  console.log("Pulling latest commit from " + req.body.repository.name);
+
+  exec('git -C ~/'+ req.body.repository.name +' reset --hard', execCallback);
+  exec('git -C ~/'+ req.body.repository.name +' clean -df', execCallback);
+  exec('git -C ~/'+ req.body.repository.name +' pull -f', execCallback);
+
+  exec('./tasks/'+ req.body.repository.name +'.sh', execCallback);
+});
+
+app.listen(5000, function () {
+  console.log('listening on port 5000')
+});
+
+function execCallback(err, stdout, stderr) {
+  if (stdout) console.log(stdout);
+  if (stderr) console.log(stderr);
+}
